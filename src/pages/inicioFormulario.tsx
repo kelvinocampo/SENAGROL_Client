ccd19e7f7e95003f5ddfa01846aeec42b4b15f02
luisa230306 <luisa03barrera@gmail.com>
2025-05-09 13:26:06 -0500

    inicioMnual

---
+++ src/pages/inicioFormulario.tsx
@@ -0,0 +1,12 @@
+
+import LoginForm from '@components/inicio/inicioform';
+
+const InicioManual = () => {
+  return (
+    <div className="bg-white min-h-screen flex items-start justify-start">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default InicioManual;
