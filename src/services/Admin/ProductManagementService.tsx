f1e0c75e24b9564e299d789642de8927a608348c
kelvinocampo <kevinocampooso@gmail.com>
2025-05-20 12:21:04 -0500

    Commit to deploy

--- src/services/Admin/ProductManagementService.tsx
+++ src/services/Admin/ProductManagementService.tsx
@@ -1,6 +1,6 @@
 // ProductManagementService.ts
 export class ProductManagementService {
-  private static API_URL = 'http://localhost:10101';
+  private static API_URL = 'https://senagrol.up.railway.app';
 
   static async getProducts() {
     const res = await fetch(`${this.API_URL}/admin/products`, {
